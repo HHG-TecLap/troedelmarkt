e780cc63bd033cce8c2111f599bd7c367d9b68b5
jateute <jateute123@gmail.com>
2022-11-14 19:05:01 +0100

    Did a few changes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const Decimal = require('decimal.js');
+
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const sequelize = new Sequelize({ // Connect to DB
     dialect: 'sqlite',
-    storage: 'data/database.sqlite'
+    storage: 'data/database.sqlite',
+    password: config['dbPassword']
 });
 
 // Test DB connection (only works in an async-Context)
@@ -32,7 +37,11 @@ Seller.init({
         primaryKey: true
     },
     balance: {
-        type: DataTypes.DECIMAL,
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    rate: {
+        type: DataTypes.STRING,
         allowNull: false
     }
 }, {
@@ -55,7 +64,12 @@ app.use(express.urlencoded({ extended: true }));
  * @returns {object} The Response Object
  */
 function createResObj(obj) {
-    return {name: obj['dataValues']['name'], id: obj['dataValues']['id'], balance: obj['dataValues']['balance']};
+    const balance = new Decimal(obj['dataValues']['balance']);
+    const rate = new Decimal(obj['dataValues']['rate']);
+
+    const provision = balance.times(rate);
+    const revenue = balance.minus(provision)
+    return {name: obj['dataValues']['name'], id: obj['dataValues']['id'], balance: obj['dataValues']['balance'], rate: obj['dataValues']['rate'], provision: provision.toString(), revenue: revenue.toString()}; // ToDo: Provision ausrechnen (provision), Händler Geld (revenue)
 }
 
 // register App routes
@@ -63,7 +77,7 @@ app.get('/', (req, res) => {
     res.send('Index')
 });
 
-// create a new Seller
+// get all Sellers
 app.get('/sellers', async (req, res) => {
     const sellers = await Seller.findAll();
     let filtered = [];
@@ -73,17 +87,17 @@ app.get('/sellers', async (req, res) => {
     res.send(filtered);
 });
 
-// get all Sellers
+// create a new Seller 
 app.post('/seller', async (req, res) => {
     if (!req.body) return res.status(400).send('Bad Request');
-    if (!req.body['name'] || !req.body['id']) return res.status(400).send('Bad Request');
+    if (!req.body['name'] || !req.body['id'] || !req.body['rate']) return res.status(400).send('Bad Request');
     if (Seller.findAll({
         where: {
             id: req.body['id']
         }
     })) return res.status(409).send('A seller with this ID already exists');
-    let seller = await Seller.create({name: req.body['name'], id: req.body['id'], balance: 0});
-    res.json(createResObj(seller));
+    let seller = await Seller.create({name: req.body['name'], id: req.body['id'], balance: '0', rate: req.body['rate']});
+    res.status(201).json(createResObj(seller));
 });
 
 // get the Seller with the specified ID
@@ -93,7 +107,7 @@ app.get('/seller/:id', async (req, res) => {
             id: req.params['id']
         }
     });
-    if (!seller) return res.status(400).send(`A seller with the id ${req.params['id']} doesn't exists`);
+    if (!seller) return res.status(404).send(`A seller with the id ${req.params['id']} doesn't exists`);
     return res.send(createResObj(seller));
 });
 
@@ -103,9 +117,14 @@ app.delete('/seller/:id', async (req, res) => {
             id: req.params['id']
         }
     });
-    if (!seller) return res.status(400).send(`A seller with the id ${req.params['id']} doesn't exists`);
+    if (seller.dataValues['balance'])
+    if (!seller) return res.status(404).send(`A seller with the id ${req.params['id']} doesn't exists`);
     await seller.destroy();
     res.send(createResObj(seller));
+});
+
+app.get('/teapot', async (req, res) => {
+    res.status(418).send('I\'m a teapot! \n I refuse the attempt to brew coffee with a teapot');
 });
 
 app.listen(8080, () => {
