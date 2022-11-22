0ad33658f31a1aaa6be9d0244d2d73f4ed7d0cb2
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2022-11-22 21:25:11 +0100

    Reinported Troedelmarkt

---
+++ frontend/TroedelMarkt/TroedelMarkt/LoginWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TroedelMarkt
+{
+    /// <summary>
+    /// Interaktionslogik für LoginWindow.xaml
+    /// </summary>
+    
+    public partial class LoginWindow : Window
+    {
+        public string Adress { get; set; }
+        public string Port { get; set; }
+        public HTTPManager? httpManager { get; set; }
+        public LoginWindow()
+        {
+            InitializeComponent();
+            Adress = "127.0.0.1";
+            Port = "3080";
+            DataContext = Adress;
+            DataContext = Port;
+            
+        }
+
+        private async void TbnLogin_Click(object sender, RoutedEventArgs e)
+        {//Databinding for adress with regex
+            try 
+            {
+                httpManager = await HTTPManager.NewAuthenticated(Adress,int.Parse(Port), PBoxPassword.Password);
+                DialogResult = true;
+            }
+            catch(Exception ex) 
+            { 
+                if(ex is UnauthorizedException)
+                {
+                    TBlockResponce.Text = "Passwort falsch";
+                }
+                else if (ex is NotFoundException)
+                {
+                    TBlockResponce.Text = "Adresse oder Port falsch";
+                }
+                else
+                {
+                    TBlockResponce.Text = $"Es ist ein unbekannter Fehler aufgetreten\n{ex.Message},\n{ex.GetType()}";
+                    MessageBox.Show($"{ex.Message},\n{ex.GetType()}");
+                }
+            }
+        }
+    }
+
+    partial class PortValidation : ValidationRule
+    {
+        public PortValidation() { }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            try
+            {
+                int input = int.Parse(value as string);
+                if (0 <= input && input <= 65535)
+                {
+                    return ValidationResult.ValidResult;
+                }
+            }
+            catch
+            {
+                
+            }
+            return new ValidationResult(false, "illegal input");
+        }
+    }
+}
---
+++ frontend/TroedelMarkt/TroedelMarkt/MainWindow.xaml.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TroedelMarkt;
+
+namespace TroedelMarkt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<TransactionItem> Transactions { get; set; } //Epty at programm start( still filled for debug)
+        public List<string> TraderIDs { get; set; } //needs to be filld with proper API data (currently silled for debug)
+
+        public HTTPManager hTTPManager { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Transactions = new List<TransactionItem>();
+            DataContext = Transactions;
+            TraderIDs = new List<string>();
+            DataContext = TraderIDs;
+            for ( int i = 0; i<10; i++ )
+            {
+                Transactions.Add(new TransactionItem($"TraderNum{i}", i * 11m - i / 13m ));
+            }
+
+            LoginWindow lgin = new LoginWindow(); //Login 
+            var result = lgin.ShowDialog();
+            if (result != true)
+            {
+                System.Windows.Application.Current.Shutdown();
+            }
+            hTTPManager = lgin.httpManager;
+            
+
+            updateTraderList();
+            updateSumm();
+        }
+
+        private void BtnDeleteElement_Click(object sender, RoutedEventArgs e)
+        {
+            if(LbTransactions.SelectedIndex != -1)
+                Transactions.RemoveAt(LbTransactions.SelectedIndex);
+            LbTransactions.Items.Refresh();
+            updateSumm();
+        }
+
+        private void BtnAddElement_Click(object sender, RoutedEventArgs e)
+        {
+            if(LbTransactions.SelectedIndex == -1)
+            {
+                decimal Value = 0m;
+                try
+                {
+                    Value = decimal.Parse(TBoxElementValue.Text);
+                }
+                catch (Exception ex) { }
+                Transactions.Add(new TransactionItem(CBTraderID.Text, 0m));
+            }
+            else 
+            { 
+                Transactions.Add(new TransactionItem(null, 0m)); 
+            }
+            LbTransactions.Items.Refresh();
+            updateSumm();
+            LbTransactions.SelectedIndex = Transactions.Count - 1;
+        }
+
+        private void BtnExitTransaction_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Sind Sie sicher?\nAlle eingetragenen Elemente werden entfernt.", "Elemente entfernen", MessageBoxButton.OKCancel, MessageBoxImage.Warning,MessageBoxResult.Cancel);
+            if (result is MessageBoxResult.OK) 
+            {
+                Transactions.Clear();
+                LbTransactions.Items.Refresh();
+                updateSumm();
+            }
+            
+        }
+
+        private void updateSumm()
+        {
+            decimal summ = 0m;
+            foreach (TransactionItem trans in  Transactions ){
+                summ += trans.Value;
+            }
+            TBlockSumm.Text = $"Summe: {summ.ToString("c")}";
+        }
+
+        private void LbTransctionsCange(object sender, SelectionChangedEventArgs e)
+        {
+            updateSumm();
+        }
+
+        private void TbUpdateBinding(object sender, TextChangedEventArgs e)
+        {
+            (sender as TextBox).GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            updateSumm();
+        }
+
+        private void BtnMakeTRansaction_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Sind Sie sicher!\nDie Transaktion kann nicht rückgängig gemacht werden.", "Transaktion durchführen", MessageBoxButton.OKCancel, MessageBoxImage.Asterisk, MessageBoxResult.Cancel);
+            if (result is MessageBoxResult.OK)
+            {
+                foreach (TransactionItem trans in Transactions)
+                {
+                    if (TraderIDs.Find(x => x == trans.Trader) == null)
+                    {
+                        MessageBox.Show("Einige Händler IDs sind nicht korrekt.\nBitte überprüfen sie die Eingabe", "Eingabe fehlerhaft", MessageBoxButton.OK, MessageBoxImage.Stop);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void BtnTraderView_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 wind = new Window1(hTTPManager);
+            wind.Owner= this;
+            wind.Show();
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            updateTraderList();
+        }
+
+        public async void updateTraderList()
+        {
+            try
+            {
+                List<Trader> tdrs = new List<Trader>(await hTTPManager.GetAllTraders());
+                TraderIDs.Clear();
+                foreach (Trader tdr in tdrs)
+                {
+                    TraderIDs.Add(tdr.TraderID);
+                }
+                TraderIDs.Sort();
+                CBTraderID.Items.Refresh();
+            }
+            catch (Exception ex) { }
+        }
+    }
+    partial class TraderIDValidation : ValidationRule
+    {
+        public string pattern { get; set; }
+        public TraderIDValidation() { }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Regex alphaNum = new Regex(pattern);//
+            string input = value as string;
+            if (alphaNum.IsMatch(input))
+            {
+                return ValidationResult.ValidResult;
+            }
+            return new ValidationResult(false, "illegal input");
+        }
+    }
+}
