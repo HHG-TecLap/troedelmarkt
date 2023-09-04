9466e7dfe17f2d8ff0fe703b40c811c09d4b4f96
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2023-09-04 12:43:39 +0200

    Adding documentation for Trader and TransactionItem

--- frontend/TroedelMarkt/TroedelMarkt/Trader.cs
+++ frontend/TroedelMarkt/TroedelMarkt/Trader.cs
@@ -2,14 +2,32 @@
 
 namespace TroedelMarkt
 {
+    /// <summary>
+    /// Class representing a Trader
+    /// </summary>
     public class Trader
     {
         private static System.Globalization.CultureInfo InvariantCulture = System.Globalization.CultureInfo.InvariantCulture;
 
+        /// <summary>
+        /// The ID ot the <see cref="Trader"/>.
+        /// </summary>
         public string TraderID { get; set; }
+        /// <summary>
+        /// The Name of the <see cref="Trader"/>.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// The amount of money the <see cref="Trader"/> has taken in.
+        /// </summary>
         public decimal Balance { get; set; }
+        /// <summary>
+        /// The Rate used to calculate the <see cref="Provision"/>.
+        /// </summary>
         public decimal ProvisionRate { get; set; }
+        /// <summary>
+        /// The <see cref="ProvisionRate"/> as percentage.
+        /// </summary>
         public decimal ProvisionRatePerc
         {
             get
@@ -21,9 +39,24 @@ namespace TroedelMarkt
                 ProvisionRate = value / 100;
             }
         }
+        /// <summary>
+        /// The amount of money the host gets, calculated using the <see cref="ProvisionRate"/>.
+        /// </summary>
         public decimal Provision { get; set; }
+        /// <summary>
+        /// The amount of money the <see cref="Trader"/> has earned.
+        /// </summary>
         public decimal Revenue { get; set; }
 
+        /// <summary>
+        /// Constructor for <see cref="Trader"/> class.
+        /// </summary>
+        /// <param name="traderID"> ID of the <see cref="Trader"/>.</param>
+        /// <param name="name"> Name of the <see cref="Trader"/>.</param>
+        /// <param name="balance"> The <see cref="Balance"/> the <see cref="Trader"/> has taken in.</param>
+        /// <param name="revenue"> The <see cref="Revenue"/> the <see cref="Trader"/> has earned.</param>
+        /// <param name="provisionRate"> The rate at which the <see cref="Provision"/> is calculated.</param>
+        /// <param name="provision"> The <see cref="Provision"/> the host gets, calculated using the <paramref name="provisionRate"/>.</param>
         public Trader(string traderID, string name, decimal balance, decimal revenue, decimal provisionRate, decimal provision)
         {
             Name = name;
@@ -46,6 +79,12 @@ namespace TroedelMarkt
             );
         }
 
+        /// <summary>
+        /// Updating <see cref="Trader"/> from <seealso cref="JsonObject"/>.
+        /// </summary>
+        /// <remarks> Will change the <see cref="Trader"/> object.</remarks>
+        /// <param name="data"> <seealso cref="JsonObject"/> that is decoded into a <see cref="Trader"/>.</param>
+        /// <returns>The updated <see cref="Trader"/></returns>
         public Trader UpdateFromJson(JsonObject data)
         {
             TraderID = data["id"]!.ToString();
@@ -57,6 +96,11 @@ namespace TroedelMarkt
             return this;
         }
 
+        /// <summary>
+        /// Converts the <see cref="Trader"/> to a <seealso cref="JsonObject"/>.
+        /// </summary>
+        /// <remarks><see cref="Trader"/> object will not be changed. Also balance, revenue, and provision are not included.</remarks>
+        /// <returns>The <see cref="Trader"/> as a <seealso cref="JsonObject"/>.</returns>
         public JsonObject ToJson()
         {
             return new JsonObject() {
--- frontend/TroedelMarkt/TroedelMarkt/TransactionItem.cs
+++ frontend/TroedelMarkt/TroedelMarkt/TransactionItem.cs
@@ -2,16 +2,35 @@
 
 namespace TroedelMarkt
 {
+    /// <summary>
+    /// Class representing a single item in a transaction.
+    /// </summary>
     public class TransactionItem
     {
+        /// <summary>
+        /// The <see cref="Trader.TraderID"/> the <see cref="TransactionItem"/> belongs to.
+        /// </summary>
         public string Trader { get; set; }
+        /// <summary>
+        /// The value of the <see cref="TransactionItem"/>
+        /// </summary>
         public decimal Value { get; set; }
+        /// <summary>
+        /// Contructor for the <see cref="TransactionItem"/>
+        /// </summary>
+        /// <param name="trader"> The <see cref="Trader.TraderID"/> the <see cref="TransactionItem"/> belongs to.</param>
+        /// <param name="value"> The value of the <see cref="TransactionItem"/></param>
         public TransactionItem(string trader, decimal value)
         {
             Trader = trader;
             Value = value;
         }
 
+        /// <summary>
+        /// Converts the <see cref="TransactionItem"/> to a <seealso cref="JsonObject"/>.
+        /// </summary>
+        /// <remarks> The <see cref="TransactionItem"/> will not be changed.</remarks>
+        /// <returns>The <see cref="TransactionItem"/> as a <seealso cref="JsonObject"/></returns>
         public JsonObject ToJson()
         {
             return new JsonObject(){
