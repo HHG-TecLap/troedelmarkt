09b9dab08b3b9478c53633c860abf03330873e5e
albrechtf0 <fel.albrecht@heinrichheinedo.de>
2023-09-08 10:49:56 +0200

    Adding documentation to the LoginWindow

--- frontend/TroedelMarkt/TroedelMarkt/LoginWindow.xaml.cs
+++ frontend/TroedelMarkt/TroedelMarkt/LoginWindow.xaml.cs
@@ -17,14 +17,26 @@ using System.Windows.Shapes;
 namespace TroedelMarkt
 {
     /// <summary>
-    /// Interaktionslogik für LoginWindow.xaml
+    /// Interactionlogic for LoginWindow.xaml
     /// </summary>
-    
     public partial class LoginWindow : Window
     {
+        /// <summary>
+        /// The address of the server to connect to
+        /// </summary>
         public string Adress { get; set; }
+        /// <summary>
+        /// The port of the server to connect to
+        /// </summary>
         public string Port { get; set; }
+        /// <summary>
+        /// The <see cref="HTTPManager"/> for hadeling the connection to the server
+        /// </summary>
         public HTTPManager? httpManager { get; set; }
+
+        /// <summary>
+        /// Constructor for initialising the window
+        /// </summary>
         public LoginWindow()
         {
             InitializeComponent();
@@ -35,6 +47,9 @@ namespace TroedelMarkt
             
         }
 
+        /// <summary>
+        /// Funtion for handeling a clock on the login button
+        /// </summary>
         private async void TbnLogin_Click(object sender, RoutedEventArgs e)
         {//Databinding for adress with regex
             try 
@@ -61,10 +76,17 @@ namespace TroedelMarkt
         }
     }
 
+    /// <summary>
+    /// Class for validating ports
+    /// </summary>
     partial class PortValidation : ValidationRule
     {
         public PortValidation() { }
-
+        /// <summary>
+        /// Function that validates the port
+        /// </summary>
+        /// <param name="value">The port to validate</param>
+        /// <returns>The <see cref="ValidationResult"/></returns>
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
             try
